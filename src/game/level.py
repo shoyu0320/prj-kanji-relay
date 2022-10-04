996a9d429a019d245f7562595e636a112f3e62d7
shoyu <49465199+shoyu0320@users.noreply.github.com>
2022-10-04 15:53:43 +0900

    複数のデータ型に対応させました

--- src/game/level.py
+++ src/game/level.py
@@ -4,13 +4,15 @@ import yaml
 
 
 class JukugoList:
-    level2dir: Dict[str, str] = {"normal": "../../data/niji-jukugo_list.yml"}
-    # level2dir: Dict[str, str] = {"normal": "../../data/niji-jukugo.yml"}
+    level2dir: Dict[str, str] = {
+        "normal": {"jdir": "../../data/niji-jukugo_list.yml", "jtype": "dict"},
+        "easy": {"jdir": "../../data/niji-jukugo.yml", "jtype": "list"},
+    }
 
     def __init__(self):
         self.level: Dict[str, List[str]] = {}
         for k, v in self.level2dir.items():
-            j_list: List[str] = list(self._get_jukugo_list(v).keys())
+            j_list: List[str] = self._get_jukugo(**v)
             self.level[k] = self.clean_list(j_list)
 
     def clean_list(self, j_list: List[str]) -> List[str]:
@@ -22,7 +24,20 @@ class JukugoList:
             new_set.add(j)
         return list(new_set)
 
+    def _get_jukugo(self, jdir: str, jtype: str = "dict") -> List[str]:
+        if jtype == "dict":
+            return self._get_jukugo_dict(jdir)
+        elif jtype == "list":
+            return self._get_jukugo_list(jdir)
+
     def _get_jukugo_list(self, jukugo_dir: str) -> List[str]:
         with open(jukugo_dir) as f:
             data: List[str] = yaml.safe_load(f)
         return data
+
+    def _get_jukugo_dict(self, jukugo_dir) -> List[str]:
+        with open(jukugo_dir) as f:
+            data: Dict[str, Dict[str]] = yaml.safe_load(f)
+            list_data: List[str] = list(data.keys())
+        return list_data
+
