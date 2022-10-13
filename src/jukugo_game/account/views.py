9d0d3a2adc90dc51fd13392e29ab08f57ba5a1ee
shoyu <49465199+shoyu0320@users.noreply.github.com>
2022-10-13 16:01:35 +0900

    ゲストを削除し、型定義を行った

--- src/jukugo_game/account/views.py
+++ src/jukugo_game/account/views.py
@@ -1,14 +1,21 @@
-from typing import Any, Callable
+import uuid
+from typing import Any, Callable, Dict, Tuple, TypeVar
 
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.db import models
 from django.forms import ModelForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
 from .forms import LoginForm, SignupForm
 from .models import SpecialUser
+
+_Q = TypeVar("_Q", bound=models.QuerySet)
+_A = TypeVar("_A", bound=Tuple[Any, ...])
+_K = TypeVar("_K", bound=Dict[str, Any])
+GUEST = uuid.uuid4
 
 
 class IndexView(TemplateView):
@@ -26,7 +33,7 @@ class IndexView(TemplateView):
 class SignupView(CreateView):
     template_name: str = "account/signup.html"
     form_class: ModelForm = SignupForm
-    success_url: Callable[[str], Any] = reverse_lazy("account:user")
+    success_url: Callable[[str], Any] = reverse_lazy("home")
 
     def form_valid(self, form):
         result = super().form_valid(form)
@@ -42,6 +49,13 @@ class SuperLoginView(LoginView):
 
 class SuperLogoutView(LogoutView):
     template_name: str = "account/logout.html"
+    context_object_name = "player"
+    model = SpecialUser
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["players"] = SpecialUser.objects.all()
+        return context
 
 
 class SuperUserView(LoginRequiredMixin, TemplateView):
@@ -49,19 +63,3 @@ class SuperUserView(LoginRequiredMixin, TemplateView):
     success_url: Callable[[str], Any] = reverse_lazy("account:user")
     context_object_name = "player"
     model = SpecialUser
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["player"] = self.request.user
-        return context
-
-
-class GuestView(LoginRequiredMixin, TemplateView):
-    template_name: str = "account/guest.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["users"] = SpecialUser.objects.exclude(
-            username=self.request.user.username
-        )
-        return context
