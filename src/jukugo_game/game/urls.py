b5a65a39535eb0d16f836197327f8219618e4590
shoyu <49465199+shoyu0320@users.noreply.github.com>
2022-10-19 12:15:00 +0900

    detailのリンクを反映させるために順番をかえました。levelの方が強かったようです。

--- src/jukugo_game/game/urls.py
+++ src/jukugo_game/game/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "game"
 urlpatterns = [
     path("", views.GameStartView.as_view(), name="start"),
+    path("detail/", views.GameDetailView.as_view(), name="detail"),
     path("<str:level>/", views.GamePlayView.as_view(), name="play"),
     path("<str:level>/win/", views.GameWinView.as_view(), name="win",),
     path("<str:level>/lose/", views.GameLoseView.as_view(), name="lose"),
-    path("detail/", views.GameDetailView.as_view(), name="detail",),
 ]
